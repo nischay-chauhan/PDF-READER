90dd71dae9c410c41e3b08229cacd8fcada88713
nischay-chauhan <masternischay@outlook.com>
2024-03-23 16:21:26 +0530

    COmpleted Image Extraction

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from "express"
+import bodyParser from "body-parser"
+import dotenv from "dotenv"
+import imageRoute from "./routes/Image.route.js"
+const app = express()
+dotenv.config()
+app.use(bodyParser.json())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3001
+
+app.use("/api/image", imageRoute)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`)
+})
+
