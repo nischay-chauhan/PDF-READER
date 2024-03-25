bad3f158592bbb3ad764f779e19a5f0f6d22ab36
nischay-chauhan <masternischay@outlook.com>
2024-03-25 11:13:20 +0530

    Added route and HomePage

---
+++ client/src/component/Sidebar.jsx
@@ -0,0 +1,16 @@
+const Sidebar = () => {
+  return (
+    <div>
+        <div className="bg-gray-800 text-white h-full w-64 p-4">
+<h1 className="text-2xl font-semibold mb-4">Sidebar</h1>
+<ul>
+  <li className="py-2">Link 1</li>
+  <li className="py-2">Link 2</li>
+  <li className="py-2">Link 3</li>
+</ul>
+</div>
+    </div>
+  )
+}
+
+export default Sidebar
